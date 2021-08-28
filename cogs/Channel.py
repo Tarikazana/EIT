2961cfa6371e4ae3fc82fb1fa744d9c61e9f96c5
Tari Kazana <77247938+Tarikazana@users.noreply.github.com>
2021-08-28 02:13:57 +0200

    Update Channel.py

--- cogs/Channel.py
+++ cogs/Channel.py
@@ -8,16 +8,6 @@
 import discord
 import json
 from discord.ext import commands
-from datetime import datetime"""
-> Channel.py
-> Author: Tari Kazana
-> setting log channel
-
-"""
-
-import discord
-import json
-from discord.ext import commands
 from datetime import datetime
 
 
@@ -26,6 +16,9 @@ class Channel(commands.Cog):
         self.bot = bot
     
     @commands.command(name = "channel", aliases=["setchannel"])
+    @commands.guild_only()
+    @commands.has_permissions(administrator = True)
+    @commands.cooldown(1, 2, commands.BucketType.member)
     async def  commandName(self, ctx:commands.Context,channel:discord.TextChannel):
         guild = ctx.message.guild
 
@@ -38,27 +31,6 @@ class Channel(commands.Cog):
         jsonFile.close()
 
         await ctx.reply(f"Changed log channel to {channel.mention}")
-
-
-def setup(bot:commands.Bot):
-    bot.add_cog(Channel(bot))
-
-
-class Channel(commands.Cog):
-    def __init__(self, bot:commands.Bot):
-        self.bot = bot
-    
-    @commands.command(name = "channel", aliases=["setchannel"])
-    async def  commandName(self, ctx:commands.Context,channel:discord.TextChannel):
-        guild = ctx.message.guild
-
-        idDict = "{"+f'"log-channel":"{channel.id}"'+"}"
-
-        jsonString = json.dumps(idDict)
-        jsonString = jsonString.replace('\\"','"')[1:-1]
-        jsonFile = open(f"eit/server_configs/{guild.id}_config.json", "w")
-        jsonFile.write(jsonString)
-        jsonFile.close()
 
 
 def setup(bot:commands.Bot):
